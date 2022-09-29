d0f503460aaaf11581bfa6ee737dcd539f45b535
Austin Westbury <austinwestbury@live.com>
2022-09-29 13:07:33 -0400

    backend restructured

--- server/routes/posts.ts
+++ server/routes/posts.ts
@@ -1,92 +1,31 @@
 const router = require("express").Router();
-const Post = require("../Models/Post");
-const User = require("../Models/User");
 const { checkJwt, checkJwt2 } = require("./middleware");
+const posts = require('../controllers/posts.ts');
 import { Request, Response } from "express";
-import IUser from "../Models/IUser";
 
-interface UserAuth extends Request {
-  auth: {
-    userId: string;
-  };
-  currUser: IUser;
-}
-
-//ADD A POST (with jwt, recognizes which user created the post)
-router.post(
-  "/",
-  [checkJwt, checkJwt2],
-  async (req: UserAuth, res: Response) => {
-    // const userId = req.auth.payload.sub.split("|")[1]
-    try {
-      const post = new Post({
-        name: req.body.name,
-        rating: req.body.rating,
-        genre: req.body.genre,
-        userId: req.auth.userId,
-        image: req.body.image.base64,
-      });
-      //TODO: add error handling
-      await post.save();
-      console.log(req.body);
-      res.status(201).json(post);
-    } catch (error) {
-      console.log(error);
-      res.sendStatus(400);
-    }
-  }
-);
-
-//delete a post
-router.delete("/post/delete/:id", async (req: UserAuth, res: Response) => {
-  try {
-    const result = await Post.findByIdAndDelete(req.params.id);
-    res.json(result);
-  } catch (error) {
-    console.log(error);
-  }
+router.post("/", [checkJwt, checkJwt2], (req: Request, res: Response) => {
+  posts.addPost(req, res)
 });
 
-//like and dislake  a post
-router.put("/:id/like", async (req: UserAuth, res: Response) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    if (!post.likes.includes(req.body.userId)) {
-      await post.updateOne({ $push: { likes: req.body.userId } });
-      res.status(200).json("The post has been liked");
-    } else {
-      await post.updateOne({ $pull: { likes: req.body.userId } });
-      res.status(200).json("The post has been disliked");
-    }
-  } catch (error) {
-    res.status(500).json(error);
-  }
+router.delete("/post/delete/:id", (req: Request, res: Response) => {
+  posts.deletePost(req, res)
 });
 
-//get a post
-router.get("/:id", async (req: UserAuth, res: Response) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    res.status(200).json(post);
-  } catch (error) {
-    res.status(500).json(error);
-  }
+router.put("/:id/like", (req: Request, res: Response) => {
+  posts.likeDislikePost(req, res)
 });
 
-router.get("/mainfeed/:userId", async (req: UserAuth, res: Response) => {
-  try {
-    const currUser = await User.findById({ _id: req.params.userId});
-    const userPosts = await Post.find({ userId: req.params.userId });
-    const friendPosts = await Promise.all(
-      currUser.following.map((friendId: string) => {
-        //this will return each post
-        return Post.find({ userId: friendId });
-      })
-    );
-    res.json(userPosts.concat(...friendPosts));
-  } catch (error) {
-    res.status(500).json(error);
-  }
+router.get("/allposts", [checkJwt, checkJwt2], (req: Request, res: Response) => {
+  posts.getAllPosts(req, res)
 });
+
+router.get("/:id", (req: Request, res: Response) => {
+  posts.getPost(req, res)
+});
+
+router.get("/mainfeed/:userId", (req: Request, res: Response) => {
+  posts.getMainfeed(req, res)
+});
+
 
 module.exports = router;
--- server/routes/users.ts
+++ server/routes/users.ts
@@ -1,26 +1,6 @@
 const router = require("express").Router();
-const User = require("../Models/User");
 const { checkJwt, checkJwt2 } = require("./middleware");
-const Post = require("../Models/Post");
-import { Request, Response } from "express";
-import IUser from "../Models/IUser";
-
-interface UserAuth extends Request {
-  auth: {
-    userId: string;
-  };
-  currUser: IUser;
-}
-
-//get the users posts with Authentication -- TODO: should probably go to posts route!
-router.get(
-  "/posts",
-  [checkJwt, checkJwt2],
-  async (req: UserAuth, res: Response) => {
-    const posts = await Post.find({ userId: req.auth.userId });
-    res.status(200).json(posts);
-  }
-);
+const users = require('../controllers/users')
 
 //TODO: GET A USER -> will be used in the search
 //get a user
@@ -37,75 +17,12 @@ router.get(
 //     }
 // });
 
-router.get("/", async (req: Request, res: Response) => {
-  const userId = req.query.userId;
-  const username = req.query.username;
-  try {
-    const user = userId
-      ? await User.findById(userId)
-      : await User.findOne({ username: username });
-    //take away what we dont want to see
-    const { password, email, isAdmin, ...other } = user._doc;
-    res.status(200).json(other);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.get("/", users.getUser);
 
 //follow a user (with JWT) TODO: implement in frontend
-router.put(
-  "/:id/follow",
-  [checkJwt, checkJwt2],
-  async (req: UserAuth, res: Response) => {
-    //if it is not the same user
-    if (req.auth.userId !== req.params.id) {
-      try {
-        const user = await User.findById(req.params.id);
-        const currUser = req.currUser;
-
-        if (!user.followers.includes(req.auth.userId)) {
-          await user.updateOne({ $push: { followers: req.auth.userId } });
-          await currUser.updateOne({ $push: { following: req.params.id } });
-          res.status(200).json("user followed");
-        } else {
-          res.status(403).json("you already follow this user");
-        }
-      } catch (error) {
-        res.status(500).json(error);
-      }
-    } else {
-      //if it is the same user
-      res.status(403).json("you can not follow yourself");
-    }
-  }
-);
+router.put("/:id/follow", [checkJwt, checkJwt2], users.followUser);
 
 //unfollow a user (with JWT) TODO: implement in frontend
-router.put(
-  "/:id/unfollow",
-  [checkJwt, checkJwt2],
-  async (req: UserAuth, res: Response) => {
-    //if it is not the same user
-    if (req.auth.userId !== req.params.id) {
-      try {
-        const user = await User.findById(req.params.id);
-        const currUser = req.currUser;
-
-        if (user.followers.includes(req.auth.userId)) {
-          await user.updateOne({ $pull: { followers: req.auth.userId } });
-          await currUser.updateOne({ $pull: { following: req.params.id } });
-          res.status(200).json("user unfollowed");
-        } else {
-          res.status(403).json("you do not follow this user");
-        }
-      } catch (error) {
-        res.status(500).json(error);
-      }
-    } else {
-      //if it is the same user
-      res.status(403).json("you can not unfollow yourself");
-    }
-  }
-);
+router.put("/:id/unfollow", [checkJwt, checkJwt2], users.unfollowUser);
 
 module.exports = router;
