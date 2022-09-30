58dcc19b78b993b2cb42ed1b37e5644145eaa10e
M <v4nd3rp03l3ns1s@gmail.com>
2022-09-30 14:40:01 -0500

    feat: signup button fully tested

--- client/src/components/SignUpButton/index.test.js
+++ client/src/components/SignUpButton/index.test.js
@@ -1,5 +1,6 @@
 import { screen, render } from "@testing-library/react";
 import SignUpButton from './index';
+import '@testing-library/jest-dom';
 
 describe('Sign-Up Button', () => {
   test('Should render a button', () => {
@@ -10,5 +11,11 @@ describe('Sign-Up Button', () => {
   test('Should contain label "Sign Up"', () => {
     render(<SignUpButton />);
     screen.getByText(/Sign Up/);
+  })
+
+  test('Should not render if authenticated', () => {
+    render(<SignUpButton isAuthenticated={true} />);
+    // eslint-disable-next-line testing-library/no-container
+    expect(screen.queryByText(/Sign Up/)).not.toBeInTheDocument();
   })
 })
