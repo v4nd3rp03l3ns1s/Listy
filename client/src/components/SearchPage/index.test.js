612977245b3ad8576f808b56bd342a8542402c2c
M <v4nd3rp03l3ns1s@gmail.com>
2022-09-30 14:42:15 -0500

    feat: search page fully tested

--- client/src/components/SearchPage/index.test.js
+++ client/src/components/SearchPage/index.test.js
@@ -8,12 +8,12 @@ describe('Search Page', () => {
   })
 
   test('Should not render children while loading', () => {
-    render(<SearchPage />);
+    render(<SearchPage isLoading={true}/>);
     screen.getByText(/Loading/);
   })
 
   test('Should render a greeting if authenticated', () => {
-    render(<SearchPage isLoading={false} />);
+    render(<SearchPage isLoading={false} isAuthenticated={true} />);
     screen.getByText(/Hello/);
   })
 })
